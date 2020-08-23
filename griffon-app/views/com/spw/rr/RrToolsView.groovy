93d852c8a4dcb6a87aea8eb1bcaadd1fd6dd2697
swalton00 <jscottwalton@gmail.com>
2020-08-23 17:00:52 -0400

    Now running with FXML input

--- griffon-app/views/com/spw/rr/RrToolsView.groovy
+++ griffon-app/views/com/spw/rr/RrToolsView.groovy
@@ -3,31 +3,46 @@ package com.spw.rr
 import griffon.core.artifact.GriffonView
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.stage.Stage
+import javafx.scene.paint.Color
 import org.codehaus.griffon.runtime.javafx.artifact.AbstractJavaFXGriffonView
 import javax.annotation.Nonnull
+import javafx.scene.control.ScrollPane
 
 @ArtifactProviderFor(GriffonView)
 class RrToolsView extends AbstractJavaFXGriffonView {
-    @MVCMember @Nonnull
-    FactoryBuilderSupport builder
+
     @MVCMember @Nonnull
     RrToolsController controller
     @MVCMember @Nonnull
     RrToolsModel model
 
-    void initUI() {
-        builder.application(title: application.configuration['application.title'],
-            sizeToScene: true, centerOnScreen: true, name: 'mainWindow') {
-            scene(fill: WHITE, width: 200, height: 60) {
-                g = gridPane {
-                    label(id: 'clickLabel', row: 0, column: 0,
-                          text: bind(model.clickCountProperty()))
-                    button(row: 1, column: 0, prefWidth: 200,
-                           griffonActionId: 'click', id: 'click')
-                }
-                connectActions(g, controller)
-                connectMessageSource(g)
-            }
-        }
+    @Override
+    public void initUI() {
+        Stage stage = (Stage) getApplication()
+                .createApplicationContainer(Collections.emptyMap());
+        stage.setTitle(getApplication().getConfiguration().getAsString("application.title"));
+        stage.setWidth(400);
+        stage.setHeight(120);
+        stage.setScene(init());
+        getApplication().getWindowManager().attach("mainWindow", stage);
     }
+
+    // build the UI
+    private Scene init() {
+        Scene scene = new Scene(new Group());
+        scene.setFill(Color.WHITE);
+        scene.getStylesheets().add("bootstrapfx.css");
+
+        ScrollPane pane = loadFromFXML();
+      //  model.inputProperty().bindBidirectional(input.textProperty());
+      //  model.outputProperty().bindBidirectional(output.textProperty());
+        ((Group) scene.getRoot()).getChildren().addAll(pane);
+        connectActions(pane, controller);
+
+        return scene;
+    }
+
 }
